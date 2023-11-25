10869fd7f1af27f762e5af44a2759f515063fc7c
thinker227 <thinker0227@gmail.com>
2023-11-25 23:27:12 +0100

    Update FunctionValue

--- src/Stifl/Interpret/Values/FunctionValue.cs
+++ src/Stifl/Interpret/Values/FunctionValue.cs
@@ -7,15 +7,15 @@ namespace Stifl.Interpret;
 /// </summary>
 /// <param name="type">The type of the function.</param>
 /// <param name="eval">A function to evaluate the value.</param>
-public sealed class FunctionValue(FuncType type, Func<Ast.Expr.Func> eval)
-    : Value<Ast.Expr.Func, FuncType>(type, eval)
+public sealed class FunctionValue(FuncType type, Func<Ast.Expr> eval)
+    : Value<Ast.Expr, FuncType>(type, eval)
 {
     /// <summary>
     /// Creates a <see cref="FunctionValue"/> with a constant pre-evaluated value.
     /// </summary>
     /// <param name="type">The type of the function.</param>
     /// <param name="func">The constant pre-evaluated value.</param>
-    public static FunctionValue Const(FuncType type, Ast.Expr.Func value)
+    public static FunctionValue Const(FuncType type, Ast.Expr value)
     {
         var x = new FunctionValue(type, () => value);
         x.Eval();
