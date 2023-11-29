52741d3620d09818adb4943e95e6e7c44995db97
thinker227 <thinker0227@gmail.com>
2023-11-29 15:59:28 +0100

    Update template and functions

--- src/Stifl.CodeInator/Functions.cs
+++ src/Stifl.CodeInator/Functions.cs
@@ -6,10 +6,24 @@ namespace Stifl.CodeInator;
 public static class Functions
 {
     [UsedImplicitly]
-    public static string Join(string separator, ScriptArray values) =>
-        string.Join(separator, values);
+    public static string TypeName(Node node, ScriptArray parents) =>
+        string.Join('.', parents.OfType<Node>().Append(node).Select(x => x.name));
 
     [UsedImplicitly]
-    public static string JoinReverse(string separator, ScriptArray values) =>
-        string.Join(separator, values.Reverse());
+    public static string MethodName(Node node, ScriptArray parents)
+    {
+        var nodes = parents
+            .OfType<Node>()
+            .Append(node) // Make sure the node is included.
+            .ToList();
+
+        // Only skip root type name if there are more nodes than the root node.
+        var names = (nodes.Count == 1
+                ? nodes
+                : nodes.Skip(1))
+            .Reverse()
+            .Select(x => x.name);
+
+        return $"Visit{string.Concat(names)}";
+    }
 }
